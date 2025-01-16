233c7b4ee694b3c26f1648a928a9bf184df53eea
FikriBagja <fikribr10@gmail.com>
2025-01-16 11:56:13 +0700

    commit

--- admin/profile.php
+++ admin/profile.php
@@ -109,14 +109,14 @@ $photos_result = mysqli_query($koneksi, $photos_query);
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="index.php">Fikri Galeri</a>
+            <a class="navbar-brand" href="#">Fikri Galeri</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse mt-2" id="navbarNav">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a href="index.php" class="nav-link">Home</a>
+                        <a href="#" class="nav-link">Home</a>
                     </li>
                     <li class="nav-item">
                         <a href="album.php" class="nav-link">Album</a>
